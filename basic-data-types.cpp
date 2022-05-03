18a4d78ee2c0e1cb6fe26c4926d2e462e0c64321
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-05-03 16:13:27 +0530

    solved some questions

---
+++ basic-data-types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a; long b; char c; float d; double e;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    cout <<  a << endl;
+    cout <<  b << endl ;
+    cout <<  c << endl;
+    printf( "%0.3f \n" , d);
+    printf( "%0.9f \n" , e);
+    return 0;
+}
