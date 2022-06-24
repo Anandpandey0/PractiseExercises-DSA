3a3b51b7bf3b1f3d8c8388b51cdfac767d971e16
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-06-25 03:28:39 +0530

    .

--- Assignment 16/Permutation.cpp
+++ Assignment 16/Permutation.cpp
@@ -1,13 +1,13 @@
 class Solution {
 private:
-    void solve(vector<int>nums,vector<vector<int>>& ans, int index){
+    void findPairs(vector<int>nums,vector<vector<int>>& ans, int index){
         if(index>=nums.size()){
             ans.push_back(nums);
         }
         for(int i = index; i<nums.size();i++){
             //Giving chance to each element at i position one by one
             swap(nums[index],nums[i]);
-            solve(nums,ans,index+1);
+            findPairs(nums,ans,index+1);
             //returning to root elemt through backtracking
             swap(nums[index],nums[i]);
 
@@ -19,7 +19,7 @@ public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
         int index = 0;
-        solve(nums,ans,index);
+        findPairs(nums,ans,index);
         return ans;
     }
 };
