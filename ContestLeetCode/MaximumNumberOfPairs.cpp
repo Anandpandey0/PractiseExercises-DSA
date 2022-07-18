63ee852b0c1d496c7a58f7cbeafd33ef9fbaff54
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2022-07-18 08:59:01 +0530

    Update MaximumNumberOfPairs.cpp

--- ContestLeetCode/MaximumNumberOfPairs.cpp
+++ ContestLeetCode/MaximumNumberOfPairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    vector<int> numberOfPairs(vector<int>& nums) {
-        unordered_map<int,int >um;
-        for(auto x:nums)um[x]++;
-        int pair=0;
-         int unpaired=0;
-        for(auto x:um){
-            pair+=x.second/2;
-            unpaired+=x.second%2;
-        }
-        vector<int>ans;
-        ans.push_back(pair);
-        ans.push_back(unpaired);
-        return ans;
+    vector<int> numberOfPairs(vector<int>& n) {
+        sort(n.begin(),n.end());
+        int a=0;
+        int b=0;
+        int i;
+        for (i=0;i<n.size()-1;i++)
+            if (n[i]==n[i+1]) {
+                a++;
+                i++;
+            } else b++;
+        if (i==n.size()-1) b++;
+        return {a,b};
     }
 };
